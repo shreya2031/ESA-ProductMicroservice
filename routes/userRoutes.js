a69ad48700e0be33367be3af70c41ebd8b9507e6
Shreya <shreya18515@mechyd.ac.in>
2021-04-19 16:52:29 +0530

    Product API

---
+++ routes/userRoutes.js
@@ -0,0 +1,15 @@
+'use strict';
+module.exports = function(app) {
+
+var user = require('../controllers/userController');
+app.route('/rest/v1/users')
+    .get(user.list_all_users)
+    .post(user.create_a_user);
+
+
+app.route('/rest/v1/users/:username')
+    .get(user.read_a_user)
+    .put(user.update_a_user)
+    .delete(user.delete_a_user);
+
+};
---
+++ server.js
@@ -0,0 +1,29 @@
+var express = require('express'),
+  app = express(),
+  port = process.env.PORT || 5000,
+  mongoose = require('mongoose'),
+
+  User = require('./models/userModel'), 
+  ProductItem = require('./models/ProductModel'),
+  bodyParser = require('body-parser');
+  require("dotenv").config();
+
+//MongoDB Atlas connection url
+const uri = process.env.mongo_URI;
+
+mongoose.connect(uri,{useNewUrlParser:true,useUnifiedTopology: true,useCreateIndex:true,useFindAndModify:false});
+
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+//Routes
+var userRoutes = require('./routes/userRoutes'); 
+var productRoutes = require('./routes/productRoutes');
+//Connecting it to the express module
+userRoutes(app); 
+productRoutes(app);
+
+app.listen(port);
+
+console.log('Product API server running on port : ' + port);
+
+
